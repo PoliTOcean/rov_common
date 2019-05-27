d7cab21ace75a22a02477ea7fa5e5e9594789ed4
pettinz <angelo.pettinelli@icloud.com>
2019-05-27 18:12:45 +0200

    Update

--- RovCommonConfig.cmake
+++ RovCommonConfig.cmake
@@ -1,3 +1,4 @@
+
 ####### Expanded from @PACKAGE_INIT@ by configure_package_config_file() #######
 ####### Any changes to this file will be overwritten by the next CMake run ####
 ####### The input file was Config.cmake.in                            ########
